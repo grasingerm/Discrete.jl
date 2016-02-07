1c7aa7065520b72c12acdfb54c5517087a2cada7
Matthew Grasinger <grasingerm@gmail.com>
2016-02-07 00:17:45 -0500

    wrote basic forward, backward, and central difference functions

--- src/Discrete.jl
+++ src/Discrete.jl
@@ -1,5 +1,163 @@
 module Discrete
 
-# package code goes here
+export ConstSequence, Sequence, DFun;
+export fdiff, bdiff, cdiff;
 
-end # module
+import FastAnonymous;
+
+immutable ConstSequence
+  i_start::Int;
+  values::Vector{Float64};
+
+  Sequence(i_start::Int, values::Vector{Float64}) = new(i_start, values);
+end
+
+function call(s::ConstSequence, n::Int)
+  if n < s.i_start || n > s.i_start + length(s.values)
+    error("Out of domain on which sequence is defined");
+  end
+  
+  return s.values[s.i_start + n];
+end 
+
+function call(s::ConstSequence, nreal::Real)
+  const n = convert(Int, nreal);
+  if n < s.i_start || n > s.i_start + length(s.values)
+    error("Out of domain on which sequence is defined");
+  end
+  
+  return s.values[s.i_start + n];
+end
+
+typealias DFun     Union{Function, FastAnonymous.Fun};
+typealias Sequence Union{DFun, ConstSequence};
+
+#! Compute the forward difference of a sequence
+#!
+#! \param     seq     Sequence
+#! \param     n       Index at which to take compute difference
+#! \param     order   Order of difference
+#! \return    Forward difference
+function fdiff(seq::Sequence, n::Int; order::Int=1)
+  @assert(order > 0, "unable to compute $order order forward difference");
+
+  sum = 0.0;
+  for i=0:order
+    sum += (((order-i)%2==0) ? 
+              binomial(order, i) * seq(n + i) : 
+             -binomial(order, i) * seq(n + i));
+  end
+
+  return sum;
+end
+
+#! Compute the forward difference of a sequence
+#!
+#! \param     seq     Sequence
+#! \param     n_range Range of index for which to compute differences
+#! \param     order   Order of difference
+#! \return    Forward differences
+function fdiff(seq::Sequence, n_range::UnitRange{Int}; order::Int=1)
+  return pmap(n -> fdiff(seq, n, order=order), n_range);
+end
+
+#! Compute the forward difference of a sequence
+#!
+#! \param     seq     Sequence
+#! \param     n_range Range of index for which to compute differences
+#! \param     order   Order of difference
+#! \return    Forward differences
+function fdiff(seq::Sequence, n_range::UnitRange{Float64}; order::Int=1)
+  return pmap(n -> fdiff(seq, n, order=order), 
+              convert(UnitRange{Int}, n_range));
+end
+
+#! Compute the backward difference of a sequence
+#!
+#! \param     seq     Sequence
+#! \param     n       Index at which to take compute difference
+#! \param     order   Order of difference
+#! \return    Backward difference
+function bdiff(seq::Sequence, n::Int; order::Int=1)
+  @assert(order > 0, "unable to compute $order order forward difference");
+
+  sum = 0.0;
+  for i=0:order
+    sum += (((order-i)%2==0) ? 
+              binomial(order, i) * seq(n - i) : 
+             -binomial(order, i) * seq(n - i));
+  end
+
+  return sum;
+end
+
+#! Compute the backward difference of a sequence
+#!
+#! \param     seq     Sequence
+#! \param     n_range Range of index for which to compute differences
+#! \param     order   Order of difference
+#! \return    Backward differences
+function bdiff(seq::Sequence, n_range::UnitRange{Int}; order::Int=1)
+  return pmap(n -> bdiff(seq, n, order=order), n_range);
+end
+
+#! Compute the backward difference of a sequence
+#!
+#! \param     seq     Sequence
+#! \param     n_range Range of index for which to compute differences
+#! \param     order   Order of difference
+#! \return    Backward differences
+function bdiff(seq::Sequence, n_range::UnitRange{Float64}; order::Int=1)
+  return pmap(n -> bdiff(seq, n, order=order), 
+              convert(UnitRange{Int}, n_range));
+end
+
+#! Calculate forward difference on a continuous function
+#!
+#! \param   f     Continuous function
+#! \param   x     Value to calculate difference at
+#! \param   h     Step size
+#! \param   order Order of difference
+#! \retrun  Difference
+function fdiff(f::DFun, x::Real, h::Real; order=1)
+  sum = 0.0;
+  for i=0:order
+    sum += ((i % 2 == 0) ? binomial(order, i) * f(x + (n - i) * h) :
+                          -binomial(order, i) * f(x + (n - i) * h));
+  end
+  return sum;
+end
+
+#! Calculate backward difference on a continuous function
+#!
+#! \param   b     Continuous function
+#! \param   x     Value to calculate difference at
+#! \param   h     Step size
+#! \param   order Order of difference
+#! \retrun  Difference
+function bdiff(f::DFun, x::Real, h::Real; order=1)
+  sum = 0.0;
+  for i=0:order
+    sum += ((i % 2 == 0) ? binomial(order, i) * f(x - i * h) :
+                          -binomial(order, i) * f(x - i * h));
+  end
+  return sum;
+end
+
+#! Calculate central difference on a continuous function
+#!
+#! \param   b     Continuous function
+#! \param   x     Value to calculate difference at
+#! \param   h     Step size
+#! \param   order Order of difference
+#! \retrun  Difference
+function cdiff(f::DFun, x::Real, h::Real; order=1)
+  sum = 0.0;
+  for i=0:order
+    sum += ((i % 2 == 0) ? binomial(order, i) * f(x + (n/2 - i) * h) :
+                          -binomial(order, i) * f(x + (n/2 - i) * h));
+  end
+  return sum;
+end
+
+end # end module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,32 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+e = (n) -> 2.0^n;
+for i=1:10000
+  n = rand(0:1000);
+  
+  e(n) == fdiff(e, n);
+  e(n) == fdiff(e, n, order=2);
+  e(n) == fdiff(e, n, order=3);
+  e(n) == fdiff(e, n, order=4);
+  e(n) == fdiff(e, n, order=rand(1:25));
+  e(n) == bdiff(e, n);
+  e(n) == bdiff(e, n, order=2);
+  e(n) == bdiff(e, n, order=3);
+  e(n) == bdiff(e, n, order=4);
+  e(n) == bdiff(e, n, order=rand(1:25));
+end
+
+f(n)   = n^2;
+df(n)  = 2*n + 1;
+dbf(n) = 2*n - 1;
+d2f(n) = 2;
+for i=1:10000
+  n = rand(0:1000);
+  
+  df(n)  == fdiff(e, n);
+  d2f(n) == fdiff(e, n, order=2);
+  dbf(n) == bdiff(e, n);
+  d2f(n) == bdiff(e, n, order=2);
+end
